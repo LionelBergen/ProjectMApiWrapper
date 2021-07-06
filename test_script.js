e1cf149d0e22689e71a3664469d64da89bb82348
dustytrash <Lionelbergen@live.com>
2021-07-06 13:37:48 -0500

    added tes timeout function for testing

--- test_script.js
+++ test_script.js
@@ -5,7 +5,22 @@ async function foo()
 {
   const x = await projectMApi.getAllClassIds();
 
+  console.log('got all class ids:');
   console.log(x);
+  
+  await wait(2);
+  
+  
+  
+  
 }
 
 foo();
+
+
+
+function wait(timeoutInSeconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeoutInSeconds * 1000);
+  });
+}
