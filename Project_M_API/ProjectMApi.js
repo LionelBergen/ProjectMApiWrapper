41c23766c561a88043b975a42015f59498335615
dustytrash <Lionelbergen@live.com>
2021-07-06 15:05:01 -0500

    changed to static functions, fix all eslint warnings except in test_script

--- Project_M_API/ProjectMApi.js
+++ Project_M_API/ProjectMApi.js
@@ -1,26 +1,27 @@
 import HTTPSCall from './HttpsCaller.js';
 
-const PROJECT_M_API_URL = "https://flyff-api.sniegu.fr";
+const PROJECT_M_API_URL = 'https://flyff-api.sniegu.fr';
 
-class ProjectM
-{
-  constructor()
-  {
+/**
+ * Same method call for getting a class versus getting multiple classes.
+ * This function can accept an array, or a string of numbers seprated by commas
+ * E.G: [1, 2, 3], '1,2,3', '1, 2, 3'
+*/
+function getClassByIdOrIds(ids) {
+  return HTTPSCall(`${PROJECT_M_API_URL}/class/${ids}`);
+}
+
+class ProjectM {
+  static getAllClassIds() {
+    return HTTPSCall(`${PROJECT_M_API_URL}/class`);
   }
-  
-  getAllClassIds()
-  {
-    return HTTPSCall(PROJECT_M_API_URL + "/class");
+
+  static getClassById(id) {
+    return getClassByIdOrIds(id);
   }
-  
-  getClassById(id)
-  {
-    return HTTPSCall(PROJECT_M_API_URL + "/class/" + id);
-  }
-  
-  getClassesByIds(ids)
-  {
-    return this.getClassById(ids);
+
+  static getClassesByIds(ids) {
+    return getClassByIdOrIds(ids);
   }
 }
 
--- test_script.js
+++ test_script.js
@@ -1,47 +1,42 @@
 import ProjectMAPI from './Project_M_API/ProjectMApi.js';
-const projectMApi = new ProjectMAPI();
 
-async function foo()
-{
+// const projectMApi = new ProjectMAPI();
+
+async function foo() {
   let listOfClasses;
-  const allClassIds = await projectMApi.getAllClassIds();
 
-  /*
+  const allClassIds = await ProjectMAPI.getAllClassIds();
   console.log('got all class ids:');
   console.log(allClassIds);
-  
+
   await wait(2);
-  const someClass = await projectMApi.getClassById(allClassIds[0]);
+  const someClass = await ProjectMAPI.getClassById(allClassIds[0]);
   console.log('got class from id:');
   console.log(someClass);
-  
+
   await wait(2);
   var arrayOfIds = [allClassIds[0], allClassIds[1]];
-  let listOfClasses = await projectMApi.getClassById(arrayOfIds);
+  listOfClasses = await ProjectMAPI.getClassById(arrayOfIds);
   console.log('got list of classes from an array:');
   console.log(listOfClasses);
-  
+
   await wait(2);
-  listOfClasses = await projectMApi.getClassById('764,2246,9686');
+  listOfClasses = await ProjectMAPI.getClassById('764,2246,9686');
   console.log('got list of classes from a string:');
   console.log(listOfClasses);
-  
+
   await wait(2);
-  listOfClasses = await projectMApi.getClassById('764, 2246, 9686');
+  listOfClasses = await ProjectMAPI.getClassById('764, 2246, 9686');
   console.log('got list of classes from a string, with spaces:');
-  console.log(listOfClasses);*/
-  
+  console.log(listOfClasses);
+
   await wait(2);
-  listOfClasses = await projectMApi.getClassesByIds('764, 2246, 9686');
+  listOfClasses = await ProjectMAPI.getClassesByIds('764, 2246, 9686');
   console.log('getClassesByIds:');
   console.log(listOfClasses);
-  
-  
 }
 
 foo();
-
-
 
 function wait(timeoutInSeconds) {
   return new Promise(resolve => {
