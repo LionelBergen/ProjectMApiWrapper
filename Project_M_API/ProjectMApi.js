51a65a5a3a6ad04a6b1b6e2bfbb4fdfa9c9054eb
dustytrash <Lionelbergen@live.com>
2021-07-06 14:04:20 -0500

    added getClassList method

--- Project_M_API/ProjectMApi.js
+++ Project_M_API/ProjectMApi.js
@@ -15,7 +15,20 @@ class ProjectM
   
   getClassById(id)
   {
+    /*
+    let idsArgument = id;
+    
+    if (Array.isArray(id)) {
+      idsArgument = id.toString();
+    }
+    
+    */
     return HTTPSCall(PROJECT_M_API_URL + "/class/" + id);
+  }
+  
+  getClassByIds(ids)
+  {
+    return getClassById(ids);
   }
 }
 
--- test_script.js
+++ test_script.js
@@ -9,9 +9,25 @@ async function foo()
   console.log(allClassIds);
   
   await wait(2);
-  
   const someClass = await projectMApi.getClassById(allClassIds[0]);
+  console.log('got class from id:');
   console.log(someClass);
+  
+  await wait(2);
+  var arrayOfIds = [allClassIds[0], allClassIds[1]];
+  let listOfClasses = await projectMApi.getClassById(arrayOfIds);
+  console.log('got list of classes from an array:');
+  console.log(listOfClasses);
+  
+  await wait(2);
+  listOfClasses = await projectMApi.getClassById('764,2246,9686');
+  console.log('got list of classes from a string:');
+  console.log(listOfClasses);
+  
+  await wait(2);
+  listOfClasses = await projectMApi.getClassById('764, 2246, 9686');
+  console.log('got list of classes from a string, with spaces:');
+  console.log(listOfClasses);
   
   
 }
